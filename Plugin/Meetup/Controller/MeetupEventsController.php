43d44eb0b5bd036f08863c09cfba586b21dbcf64
Graham Weldon <graham@grahamweldon.com>
2011-11-01 16:17:22 +1100

    Added comments

--- Plugin/Meetup/Controller/MeetupEventsController.php
+++ Plugin/Meetup/Controller/MeetupEventsController.php
@@ -1,17 +1,43 @@
 <?php
 App::uses('MeetupAppController', 'Meetup.Controller');
 
+/**
+ * MeetupEvents Controller
+ *
+ * @package Meetup
+ * @subpackage Meetup.Controller
+ * @author Graham Weldon (http://grahamweldon.com)
+ */
 class MeetupEventsController extends MeetupAppController {
-	
+
+/**
+ * Before Filter
+ *
+ * @return void
+ * @author Graham Weldon (http://grahamweldon.com)
+ */
 	public function beforeFilter() {
 		$this->set('title_for_layout', 'Events');
 	}
-	
+
+/**
+ * Index action
+ *
+ * @return void
+ * @author Graham Weldon (http://grahamweldon.com)
+ */
 	public function index() {
 		$meetupEvents = $this->MeetupEvent->find('all', array('conditions' => array('group_urlname' => 'SydPHP')));
 		$this->set(compact('meetupEvents'));
 	}
-	
+
+/**
+ * View action
+ *
+ * @param string $id Event ID
+ * @return void
+ * @author Graham Weldon (http://grahamweldon.com)
+ */
 	public function view($id = null) {
 		if (!$id) {
 			throw new NotFoundException(__('Could not find the requested Event'));
