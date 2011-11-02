bb9fe16263dc7b327ec9389f0999d310b311bf4d
Graham Weldon <graham@grahamweldon.com>
2011-11-02 21:27:16 +1100

    Fixed incorrect encoding handling in JSON response

--- Plugin/Meetup/Model/Datasource/MeetupSource.php
+++ Plugin/Meetup/Model/Datasource/MeetupSource.php
@@ -120,7 +120,7 @@ class MeetupSource extends DataSource {
 			return false;
 		}
 
-		$result = json_decode($response->body, true);
+		$result = json_decode(utf8_encode($response->body), true);
 		if (!isset($result['results'])) {
 			$model->onError();
 			return false;
