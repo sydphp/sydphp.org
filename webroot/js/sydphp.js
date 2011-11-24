0be767296c26e0e788a375565bc97181293c3b3f
Graham Weldon <graham@grahamweldon.com>
2011-11-25 02:17:44 +1100

    Make SydPHP JS file standard format for CakePHP stuffs.

--- webroot/js/sydphp.js
+++ webroot/js/sydphp.js
@@ -1,39 +1,18 @@
-$('document').ready
-(
-	function()
-	{
-		$('.member-summary a').click
-		(
-			function(e)
-			{
-				e.preventDefault();
-				$.get
-				(
-					$(e.target).attr('href'),
-					function(response)
-					{
-						$('#window-popup').remove();
-						$(response).appendTo(document.body);
-						$('#window-popup').modal
-						(
-							{
-								show:		true,
-								backdrop:	true
-							}
-						);
-						$(document.body).keypress
-						(
-							function(e)
-							{
-								if (e.keyCode==27)
-								{
-									$('#window-popup').modal({hide:true});
-								}
-							}
-						);
-					}
-				);
-			}
-		);
-	}
-);
+$('document').ready(function() {
+	$('.member-summary a').click(function(e) {
+		e.preventDefault();
+		$.get($(e.target).attr('href'), function(response) {
+			$('#window-popup').remove();
+			$(response).appendTo(document.body);
+			$('#window-popup').modal({
+				show:		true,
+				backdrop:	true
+			});
+			$(document.body).keypress(function(e) {
+				if (e.keyCode==27) {
+					$('#window-popup').modal({hide:true});
+				}
+			});
+		});
+	});
+});
