2b7c43c68e51979a408249deec12f4ec11108a59
Graham Weldon <graham@grahamweldon.com>
2013-01-17 16:59:28 +0900

    Fix development path being included.

--- webroot/index.php
+++ webroot/index.php
@@ -56,7 +56,7 @@
  *
  * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
  */
-	define('CAKE_CORE_INCLUDE_PATH',  DS . 'Users' . DS . 'predominant' . DS . 'Projects' . DS . 'cakephp' . DS . '2.0' . DS . 'lib');
+	//define('CAKE_CORE_INCLUDE_PATH',  DS . 'Users' . DS . 'predominant' . DS . 'Projects' . DS . 'cakephp' . DS . '2.0' . DS . 'lib');
 
 /**
  * Editing below this line should NOT be necessary.
